7a7b3705b86057e06ec5103db8aa10972ac8d148
jwkim <phoongdeng@gmail.com>
2021-02-07 01:47:07 +0900

    Add real API wrapping graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,51 +1,9 @@
-let movies = [
-  {
-    id: 0,
-    name: 'Iron Man 1',
-    score: 89,
-  },
-  {
-    id: 1,
-    name: 'Iron Man 2',
-    score: 87,
-  },
-  {
-    id: 2,
-    name: 'Iron Man 3',
-    score: 90,
-  },
-  {
-    id: 3,
-    name: 'Avengers 1',
-    score: 92,
-  },
-  {
-    id: 4,
-    name: 'Avengers 2',
-    score: 97,
-  },
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.am/api/v2/list_movies.json';
 
-export const getMovieById = (id) => movies.find((movie) => id === movie.id);
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-export const deleteMovie = (id) => {
-  const filteredMovies = movies.filter((movie) => id !== movie.id);
-  if (movies.length > filteredMovies.length) {
-    movies = filteredMovies;
-    return true;
-  } else {
-    return false;
-  }
+export const getMovies = (limit, rating) => {
+  return fetch(`${API_URL}?limit=${limit}&minimum_rating=${rating}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
